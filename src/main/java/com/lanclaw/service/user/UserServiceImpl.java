75d90aed8749d479ae5e9cd3cdcc323b52f60e04
Ruihan Xu <3423406675@qq.com>
2022-01-10 19:27:29 +0800

    Initial commit

---
+++ src/main/java/com/lanclaw/service/user/UserServiceImpl.java
@@ -0,0 +1,218 @@
+package com.lanclaw.service.user;
+
+import com.lanclaw.dao.BaseDao;
+import com.lanclaw.dao.user.UserDao;
+import com.lanclaw.dao.user.UserDaoImpl;
+import com.lanclaw.pojo.User;
+
+
+import java.sql.*;
+import java.util.List;
+
+
+public class UserServiceImpl implements UserService {
+    //业务层会调用dao层，所以我们要引入Dao层
+    private UserDao userDao;
+
+    public UserServiceImpl() {
+        userDao = new UserDaoImpl();
+    }
+
+    public User login(String userCode, String userPassword) {
+        // TODO Auto-generated method stub
+        Connection connection = null;
+        User user = null;
+        try {
+            connection = BaseDao.getConnection();
+            user = userDao.getLoginUser(connection, userCode);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            BaseDao.closeResource(connection, null, null);
+        }
+
+        //匹配密码
+        if (null != user) {
+            if (!user.getUserPassword().equals(userPassword)) {
+                user = null;
+            }
+        }
+
+        return user;
+    }
+
+    public boolean updatePwd(int id, String pwd) {
+        boolean flag = false;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            if (userDao.updatePwd(connection, id, pwd) > 0) {
+                flag = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            BaseDao.closeResource(connection, null, null);
+        }
+        return flag;
+    }
+
+    public int getUserCount(String queryUserName, int queryUserRole) {
+        // TODO Auto-generated method stub
+        Connection connection = null;
+        int count = 0;
+        System.out.println("queryUserName ---- > " + queryUserName);
+        System.out.println("queryUserRole ---- > " + queryUserRole);
+        try {
+            connection = BaseDao.getConnection();
+            count = userDao.getUserCount(connection, queryUserName,queryUserRole);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            BaseDao.closeResource(connection, null, null);
+        }
+        return count;
+    }
+
+    public List<User> getUserList(String queryUserName, int queryUserRole, int currentPageNo, int pageSize) {
+        // TODO Auto-generated method stub
+        Connection connection = null;
+        List<User> userList = null;
+        System.out.println("queryUserName ---- > " + queryUserName);
+        System.out.println("queryUserRole ---- > " + queryUserRole);
+        System.out.println("currentPageNo ---- > " + currentPageNo);
+        System.out.println("pageSize ---- > " + pageSize);
+        try {
+            connection = BaseDao.getConnection();
+            userList = userDao.getUserList(connection, queryUserName,queryUserRole,currentPageNo,pageSize);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            BaseDao.closeResource(connection, null, null);
+        }
+        return userList;
+    }
+
+    public boolean add(User user) {
+        // TODO Auto-generated method stub
+
+        boolean flag = false;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            connection.setAutoCommit(false);//开启JDBC事务管理
+            int updateRows = userDao.add(connection,user);
+            connection.commit();
+            if(updateRows > 0){
+                flag = true;
+                System.out.println("add success!");
+            }else{
+                System.out.println("add failed!");
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            try {
+                System.out.println("rollback==================");
+                connection.rollback();
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        }finally{
+            //在service层进行connection连接的关闭
+            BaseDao.closeResource(connection, null, null);
+        }
+        return flag;
+    }
+
+    public boolean deleteUserById(Integer delId) {
+        // TODO Auto-generated method stub
+        Connection connection = null;
+        boolean flag = false;
+        try {
+            connection = BaseDao.getConnection();
+            if(userDao.deleteUserById(connection,delId) > 0) {
+                flag = true;
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            BaseDao.closeResource(connection, null, null);
+        }
+        return flag;
+    }
+
+    public boolean modify(User user) {
+        // TODO Auto-generated method stub
+        Connection connection = null;
+        boolean flag = false;
+        try {
+            connection = BaseDao.getConnection();
+            if(userDao.modify(connection,user) > 0) {
+                flag = true;
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            BaseDao.closeResource(connection, null, null);
+        }
+        return flag;
+    }
+
+    public User getUserById(String id) {
+        // TODO Auto-generated method stub
+        User user = null;
+        Connection connection = null;
+        try{
+            connection = BaseDao.getConnection();
+            user = userDao.getUserById(connection,id);
+        }catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            user = null;
+        }finally{
+            BaseDao.closeResource(connection, null, null);
+        }
+        return user;
+    }
+
+    public User selectUserCodeExist(String userCode) {
+        // TODO Auto-generated method stub
+        Connection connection = null;
+        User user = null;
+        try {
+            connection = BaseDao.getConnection();
+            user = userDao.getLoginUser(connection, userCode);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            BaseDao.closeResource(connection, null, null);
+        }
+        return user;
+    }
+    /*@Test
+	public void test() {
+		UserServiceImpl userService = new UserServiceImpl();
+		String userCode = "admin";
+		String userPassword = "12345678";
+		User admin = userService.login(userCode, userPassword);
+		System.out.println(admin.getUserPassword());
+
+	}*/
+    
+    /*@Test
+    public void test(){
+     UserService userService = new UserServiceImpl();
+     int userCount = userService.getUserCount(null,1);
+     System.out.println(userCount);
+    }*/
+
+}
---
+++ src/main/java/com/lanclaw/util/Constants.java
@@ -0,0 +1,8 @@
+package com.lanclaw.util;
+
+
+public class Constants {
+    public final static String USER_SESSION="userSession";
+    public final static String SYS_MESSAGE = "message";
+    public final static int pageSize = 5;
+}
